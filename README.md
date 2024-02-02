8b895a13b1048922d57e6640a6c47daa62ec408e
btmask <90320947+adriangalilea@users.noreply.github.com>
2024-02-02 16:28:14 +0100

    feat: faq

--- README.md
+++ README.md
@@ -24,6 +24,8 @@ e-ID is your digital business card.
 - [ ] feat: premium analytics
 - [ ] feat: wishlist
 - [x] feat: progressive enhancing browsers that support emoji domain
+- [x] feat: faq section
+- [ ] feat: improve faq
 
 # Testing
 ![telegram-cloud-photo-size-4-5899819325966110101-y](https://github.com/adriangalilea/e-id/assets/90320947/a06d37a0-54b9-4aec-ae14-a096bcac31f9)
---
+++ app/faq/page.tsx
@@ -0,0 +1,57 @@
+// create a faq page
+// use card component
+// use accordion component
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function Faq() {
+  return (
+    <div className="w-full">
+      <Card className="prose prose-zinc dark:prose-invert w-full">
+        <CardHeader>
+          <CardTitle>Frequently Asked Questions</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Accordion type="single" collapsible className="w-full">
+            <AccordionItem value="item-1">
+              <AccordionTrigger>What is e-id?</AccordionTrigger>
+              <AccordionContent>
+                e-id is a digital business card that allows you to share all
+                your social links and contact information in one place.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+              <AccordionTrigger>How do I get an e-id?</AccordionTrigger>
+              <AccordionContent>
+                Visit our homepage and fill-in the form to get your e-id, no
+                sign-up required.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+              <AccordionTrigger>Is e-id safe and secure?</AccordionTrigger>
+              <AccordionContent>
+                Yes, e-id is safe and secure. We literally don't store any of
+                your data. All the data is encoded in the URL and is only
+                accessible by the person who has the link.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
