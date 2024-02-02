4827eff294687c582c4e7b7fd86699536b5fb88b
btmask <90320947+adriangalilea@users.noreply.github.com>
2024-02-02 13:17:34 +0100

    style: move padding into layout

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -56,7 +56,6 @@ const keyToIconAndUrl: Record<
 export default function Page({ params }: { params: { slug: string } }) {
   const [decodedData, setDecodedData] = useState<UserProfile1 | null>(null);
 
-
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -153,11 +152,11 @@ export default function Page({ params }: { params: { slug: string } }) {
   return (
     <div className="min-h-screen w-screen">
       <main className="w-full max-w-2xl shadow-lg rounded-lg overflow-hidden">
-        <article className="shadow rounded-lg p-4 sm:py-5 sm:px-10 lg:px-16 lg:py-8 prose lg:prose-xl prose-zinc dark:prose-invert antialiased flex flex-col gap-8 w-fit">
+        <article className="shadow rounded-lg prose lg:prose-xl prose-zinc dark:prose-invert antialiased flex flex-col gap-8 w-fit">
           {decodedData ? renderDecodedData(decodedData) : <p>Loading...</p>}
         </article>
       </main>
-      <footer className="w-full prose lg:prose-xl prose-zinc dark:prose-invert p-4 sm:py-5 sm:px-10 lg:px-16 lg:py-8 fixed bottom-0">
+      <footer className="w-full prose lg:prose-xl prose-zinc dark:prose-invert fixed bottom-0">
         <Button asChild variant="link">
           <Link className="lg:no-underline" href="/">
             👤 Get your e-id
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const UserProfileForm = dynamic(() => import("../components/Form"), {
 
 export default function Home() {
   return (
-    <main className="p-4 sm:py-5 sm:px-10 lg:px-16 lg:py-8">
+    <main>
       <div className="z-10 max-w-sm w-full items-center justify-between">
         <UserProfileForm />
       </div>
