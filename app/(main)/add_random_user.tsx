81ff9bba2c9c158146189a2eca165075a97a4a8e
btmask <90320947+adriangalilea@users.noreply.github.com>
2024-03-17 00:23:30 +0100

    feat: Add user pending disabled

--- app/(main)/add_random_user.tsx
+++ app/(main)/add_random_user.tsx
@@ -2,12 +2,21 @@
 import { createRandomUser } from "@/db/actions";
 import { Button } from "@/components/ui/button";
 import { useFormState, useFormStatus } from "react-dom";
+import { Loader2, Plus } from "lucide-react";
 
 function SubmitButton() {
   const { pending } = useFormStatus();
-
   return (
-    <Button type="submit" aria-disabled={pending}>
+    <Button
+      disabled={pending}
+      type="submit"
+      className="transition-all ease-in-out duration-100"
+    >
+      {pending ? (
+        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+      ) : (
+        <Plus className="mr-2 h-4 w-4" />
+      )}
       Add user
     </Button>
   );
