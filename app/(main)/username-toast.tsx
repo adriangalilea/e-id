b08b39bc7a2b5d13d1b32e42c67d76b186d08a5b
btmask <90320947+adriangalilea@users.noreply.github.com>
2024-03-19 13:29:06 +0100

    chore: testing user toast

--- app/(main)/username-toast.tsx
+++ app/(main)/username-toast.tsx
@@ -3,15 +3,21 @@ import { useToast } from "@/components/ui/use-toast";
 import { ToastAction } from "@/components/ui/toast";
 import { useEffect } from "react";
 
-export default function UsernameToast({username}: {username: string | null | undefined}) {
+export default function UsernameToast({
+  username,
+}: {
+  username: string | null | undefined;
+}) {
+  console.log(username);
   const { toast } = useToast();
   useEffect(() => {
+    if (username) return;
     toast({
       variant: "destructive",
       title: "Uh oh! Username taken.",
       description: "Someone already had your auth provider username.",
       action: <ToastAction altText="Set username">Set username</ToastAction>,
     });
-  }, [toast]);
+  }, [toast, username]);
   return <></>;
 }
