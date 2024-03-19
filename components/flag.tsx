fe8fa055c76912ef6af2ce96ecc4ec81178cde59
btmask <90320947+adriangalilea@users.noreply.github.com>
2024-03-19 12:31:46 +0100

    feat: flag default world

--- components/flag.tsx
+++ components/flag.tsx
@@ -2,6 +2,18 @@ import { getName } from "country-list";
 import { ReactCountryFlag } from "react-country-flag";
 
 export default function Flag({ country }: { country: string }) {
+  if (country === "XX") {
+    return (
+      <span
+        role="img"
+        aria-label="unknown country"
+        title="unknown country"
+        className="text-2xl"
+      >
+        🌐
+      </span>
+    );
+  }
   return (
     <ReactCountryFlag
       svg
