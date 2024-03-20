1d3b616e4b95ee80c1f68e23e29c8d0268307d01
btmask <90320947+adriangalilea@users.noreply.github.com>
2024-03-20 18:16:05 +0100

    ignore: testing something

--- app/(main)/[username]/comment_section.tsx
+++ app/(main)/[username]/comment_section.tsx
@@ -15,7 +15,7 @@ export default async function CommentSection({
   visitorUserId: string | null | undefined;
 }) {
   if (!visitorUserId) return null;
-
+  console.log("whatever")
   const createCommentFromFormWithID = createCommentFromForm.bind(
     null,
     profileUserId,
