efbe1042135ccda9c7561d611876983dd485320f
btmask <90320947+adriangalilea@users.noreply.github.com>
2024-03-22 18:49:53 +0100

    style: comment input when non logged in

--- app/(main)/[username]/comment_section.tsx
+++ app/(main)/[username]/comment_section.tsx
@@ -6,6 +6,7 @@ import Comment from "@/components/comment";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SendHorizontal } from "lucide-react";
+import { signIn } from "@/auth";
 // import { useState } from "react";
 
 export default async function CommentSection({
@@ -30,7 +31,7 @@ export default async function CommentSection({
 
   return (
     <div className="relative max-h-72 overflow-auto">
-      {createCommentFromFormWithID && (
+      {createCommentFromFormWithID ? (
         <form
           action={createCommentFromFormWithID}
           className="sticky top-0 z-10"
@@ -51,7 +52,32 @@ export default async function CommentSection({
             </Button>
           </div>
         </form>
+      ) : (
+        <form
+          className="sticky top-0 z-10"
+          action={async () => {
+            "use server";
+            await signIn("github");
+          }}
+        >
+          <div className="flex items-center">
+            <Input
+              type="text"
+              name="body"
+              className="rounded-none bg-zinc-200/60 text-[16px] backdrop-blur-md focus-visible:border-zinc-500 focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0 dark:bg-zinc-700/80 sm:dark:bg-zinc-800/60"
+              placeholder="I want everyone to know..."
+            />
+            <Button
+              type="submit"
+              className="z-10 rounded-none"
+              variant="secondary"
+            >
+              <SendHorizontal size={18} strokeWidth={1} />
+            </Button>
+          </div>
+        </form>
       )}
+
       <div className="flex flex-col gap-2 overflow-auto py-2 pb-12">
         {allCommentsAndCommentators.map((commentAndCommentator) => (
           <div key={commentAndCommentator.comments?.id}>
