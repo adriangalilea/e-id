2e0fe79c216b10bba34752e327557c3ae283a01e
btmask <90320947+adriangalilea@users.noreply.github.com>
2024-03-23 12:29:30 +0100

    feat: remove footer complexity

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,61 +6,51 @@ import { Separator } from "@/components/ui/separator";
 
 export default function Footer({ children }: { children?: React.ReactNode }) {
   return (
-    <footer className="fixed bottom-0 w-screen border-t-[0.1px] border-black/15 bg-zinc-50/60 text-zinc-500 shadow-2xl backdrop-blur-2xl dark:border-white/15 dark:bg-zinc-950/60 ">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center">
-          <Button asChild variant="ghost" className="rounded-none">
-            <Link href="/">
-              <Home strokeWidth={1} />
-            </Link>
-          </Button>
-          <Separator
-            orientation="vertical"
-            className="px-0! h-full py-5 sm:hidden"
-          />
-          {children}
-        </div>
-        <div className="flex">
-          <ButtonGithub />
-          <Separator
-            orientation="vertical"
-            className="px-0! h-full py-5 sm:hidden"
-          />
-          <ButtonFAQ />
-        </div>
+    <footer className="fixed bottom-0 flex justify-between border-t-[0.1px] border-black/15 bg-zinc-50/60 text-zinc-500 shadow-2xl backdrop-blur-2xl dark:border-white/15 dark:bg-zinc-950/60 ">
+      <div className="flex items-center">
+        <ButtonHome />
+        <Separator
+          orientation="vertical"
+          className="px-0! h-full py-5 sm:hidden"
+        />
+        {children}
+      </div>
+      <div className="flex">
+        <ButtonGithub />
+        <Separator
+          orientation="vertical"
+          className="px-0! h-full py-5 sm:hidden"
+        />
+        <ButtonFAQ />
       </div>
     </footer>
   );
 }
 
-function ButtonGithub({ description = false }: { description?: boolean }) {
+function ButtonHome() {
   return (
     <Button asChild variant="ghost" className="rounded-none">
-      <Link
-        className="font-light opacity-90 hover:decoration-inherit hover:opacity-100 lg:decoration-transparent"
-        href="https://github.com/adriangalilea/e-id"
-        target="_blank"
-      >
-        <Github strokeWidth={1} />
-        {description && (
-          <>
-            <span className="ml-0.5">Source code</span>
-            <ExternalLink strokeWidth={1} className="ml-0.5" />
-          </>
-        )}
+      <Link href="/">
+        <Home strokeWidth={1} />
       </Link>
     </Button>
   );
 }
-function ButtonFAQ({ description = false }: { description?: boolean }) {
+
+function ButtonGithub() {
   return (
     <Button asChild variant="ghost" className="rounded-none">
-      <Link
-        className="font-light hover:decoration-inherit hover:opacity-100 lg:decoration-transparent"
-        href="/faq"
-      >
+      <Link href="https://github.com/adriangalilea/e-id" target="_blank">
+        <Github strokeWidth={1} />
+      </Link>
+    </Button>
+  );
+}
+function ButtonFAQ() {
+  return (
+    <Button asChild variant="ghost" className="rounded-none">
+      <Link href="/faq">
         <HelpCircle strokeWidth={1} />
-        {description && <span className="ml-1">FAQ</span>}
       </Link>
     </Button>
   );
