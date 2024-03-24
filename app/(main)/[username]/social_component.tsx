bb8676dbdb22976ee76a8c1f2a52b131fbd4db5c
btmask <90320947+adriangalilea@users.noreply.github.com>
2024-03-24 12:12:12 +0100

    style: SocialComponent

--- app/(main)/[username]/social_component.tsx
+++ app/(main)/[username]/social_component.tsx
@@ -1,5 +1,18 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 import { ReactElement } from "react";
 import Link from "next/link";
+import { SelectUser } from "@/db/schema";
 import {
   Twitter,
   Facebook,
@@ -7,81 +20,144 @@ import {
   Github,
   Link as LinkIcon,
   Youtube,
+  Mail,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-export type SocialNetwork =
-  | "twitter"
-  | "facebook"
-  | "instagram"
-  | "github"
-  | "youtube"
-  | "custom_website"
-  | "personal_website";
+export const SocialNetworkMap = {
+  email: "email",
+  website: "website",
+  gh_username: "github",
+  youtube: "youtube",
+  twitter: "twitter",
+  // instagram: "instagram",
+  // facebook: "facebook",
+  // custom_website: "custom_website",
+} as const;
+
+// Infer DbSocialKeys and SocialNetwork types from SocialNetworkMap
+type DbSocialKeys = keyof typeof SocialNetworkMap;
+type SocialNetwork = (typeof SocialNetworkMap)[DbSocialKeys];
+
+// type ValidSocial = {
+//   id: SocialNetwork;
+//   url: string;
+//   icon: string;
+//   displayText: string;
+// };
 
 export function getSocialUrl(
   network: SocialNetwork,
   identifier: string,
 ): string {
   const baseUrlMap = {
-    twitter: `https://twitter.com/${identifier}`,
-    facebook: `https://facebook.com/${identifier}`,
-    instagram: `https://instagram.com/${identifier}`,
+    email: `mailto:${identifier}`,
+    website: identifier,
     github: `https://github.com/${identifier}`,
     youtube: `https://youtube.com/@${identifier}`,
-    custom_website: identifier,
-    personal_website: identifier,
+    twitter: `https://twitter.com/${identifier}`,
+    // facebook: `https://facebook.com/${identifier}`,
+    // instagram: `https://instagram.com/${identifier}`,
+    // custom_website: identifier,
   };
   return baseUrlMap[network] || identifier;
-}
-
-function getSocialIcon(network: SocialNetwork): ReactElement {
-  const iconMap = {
-    twitter: <Twitter size={18} strokeWidth={1} className="opacity-80" />,
-    facebook: <Facebook size={18} strokeWidth={1} className="opacity-80" />,
-    instagram: <Instagram size={18} strokeWidth={1} className="opacity-80" />,
-    github: <Github size={18} strokeWidth={1} className="opacity-80" />,
-    youtube: <Youtube size={18} strokeWidth={1} className="opacity-80" />,
-    custom_website: (
-      <LinkIcon size={18} strokeWidth={1} className="opacity-80" />
-    ),
-    personal_website: (
-      <LinkIcon size={18} strokeWidth={1} className="opacity-80" />
-    ),
-  };
-  return iconMap[network];
 }
 
 function getSocialDisplayText(
   network: SocialNetwork,
   identifier: string,
 ): string {
-  if (network === "custom_website" || network === "personal_website") {
+  //network === "custom_website" ||
+  if (network === "website") {
     return identifier.replace(/^https?:\/\//, "");
   }
   return `@${identifier}`;
 }
 
-interface SocialComponentProps {
-  network: SocialNetwork;
-  identifier: string;
+function getSocialIcon(network: SocialNetwork): ReactElement {
+  const iconMap = {
+    website: <LinkIcon size={18} strokeWidth={1} className="opacity-80" />,
+    email: <Mail size={18} strokeWidth={1} className="opacity-80" />,
+    github: <Github size={18} strokeWidth={1} className="opacity-80" />,
+    twitter: <Twitter size={18} strokeWidth={1} className="opacity-80" />,
+    youtube: <Youtube size={18} strokeWidth={1} className="opacity-80" />,
+    instagram: <Instagram size={18} strokeWidth={1} className="opacity-80" />,
+    facebook: <Facebook size={18} strokeWidth={1} className="opacity-80" />,
+    custom_website: (
+      <LinkIcon size={18} strokeWidth={1} className="opacity-80" />
+    ),
+  };
+  return iconMap[network];
 }
 
-export function SocialComponent({
-  network,
-  identifier,
-}: SocialComponentProps): JSX.Element {
-  const url = getSocialUrl(network, identifier);
-  const icon = getSocialIcon(network);
-  const displayText = getSocialDisplayText(network, identifier);
+type UserSocialInfo = {
+  id: DbSocialKeys; // Ensure this matches the keys from SocialNetworkMap
+  value: string | null | undefined; // Accommodate for the possibility of null values
+};
+
+// Adjust the ValidSocial type if necessary to ensure compatibility
+type ValidSocial = {
+  id: SocialNetwork;
+  url: string;
+  icon: string;
+  displayText: string;
+};
+function getValidSocials(user: SelectUser): ValidSocial[] {
+  console.log(user);
+  // Map user properties to their respective social network identifiers.
+  const mappings: Array<{ id: SocialNetwork; key: keyof SelectUser }> = [
+    { id: "email", key: "email" },
+    { id: "website", key: "website" },
+    { id: "github", key: "gh_username" },
+    { id: "youtube", key: "youtube" },
+    { id: "twitter", key: "twitter" },
+  ];
+
+  return mappings.reduce<ValidSocial[]>((acc, { id, key }) => {
+    const value = user[key];
+    if (typeof value === "string") {
+      // Ensure the value is a string to proceed.
+      const url = getSocialUrl(id, value);
+      const icon = getSocialIcon(id);
+      const displayText = getSocialDisplayText(id, value);
+      acc.push({ id, url, icon, displayText });
+    }
+    return acc;
+  }, []);
+}
+
+export function SocialComponent({ user }: { user: SelectUser }): JSX.Element {
+  const validSocials = getValidSocials(user);
+  console.log(validSocials);
+
   return (
-    <Button variant="outline" className="rounded-none">
-      <Link
-        href={url}
-        className="flex items-center gap-1 font-light no-underline"
-      >
-        {icon}
-      </Link>
-    </Button>
+    <Tabs defaultValue={validSocials[0]?.id} className="w-full">
+      <TabsList className="grid auto-cols-fr grid-flow-col rounded-none border-x border-t !p-0">
+        {validSocials.map(({ id, icon }) => (
+          <TabsTrigger
+            key={id}
+            value={id}
+            className="flex h-full justify-center rounded-none px-2"
+          >
+            {icon}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+      {validSocials.map(({ id, url, displayText }) => (
+        <TabsContent key={id} value={id} className="mt-0">
+          <Card className="rounded-none border-t-transparent">
+            <CardContent className="space-y-2 p-0">
+              <Link href={url} className="flex items-center no-underline">
+                <Button variant="ghost" className="w-full h-full rounded-none">
+                  <div className="flex flex-col items-center space-y-2">
+                    {displayText}
+                  </div>
+                </Button>
+              </Link>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      ))}
+    </Tabs>
   );
 }
--- app/(main)/[username]/user_profile.tsx
+++ app/(main)/[username]/user_profile.tsx
@@ -1,39 +1,17 @@
 import Flag from "@/components/flag";
 import { SelectUser } from "@/db/schema";
 import { SocialComponent } from "./social_component";
-import { type SocialNetwork } from "./social_component";
-
-function renderSocialLinks(user: SelectUser) {
-  const socialNetworks = [
-    { key: "github", value: user.gh_username, network: "github" },
-    { key: "twitter", value: user.twitter, network: "twitter" },
-    { key: "website", value: user.website, network: "personal_website" },
-    { key: "youtube", value: user.youtube, network: "youtube" },
-  ];
-
-  return socialNetworks
-    .filter(
-      (social): social is { key: string; value: string; network: string } =>
-        !!social.value,
-    )
-    .map((social) =>
-      SocialComponent({
-        network: social.network as SocialNetwork,
-        identifier: social.value,
-      }),
-    );
-}
 
 export default function UserProfile({ user }: { user: SelectUser }) {
   return (
     <main>
       <div>
-        <div className="prose prose-zinc flex flex-col justify-between gap-1 dark:prose-invert sm:flex-row sm:items-end sm:gap-3">
+        <div className="prose prose-zinc dark:prose-invert flex flex-col justify-between gap-1 sm:flex-row sm:items-end sm:gap-3">
           <h1 className="!m-0 text-2xl font-normal sm:font-normal">
             {user.name}
           </h1>
           <div className="flex justify-between sm:grow">
-            <p className="prose prose-zinc !m-0 font-extralight dark:prose-invert">
+            <p className="prose prose-zinc dark:prose-invert !m-0 font-extralight">
               @{user.username}
             </p>
             <div className="flex content-between items-center gap-1">
@@ -45,12 +23,12 @@ export default function UserProfile({ user }: { user: SelectUser }) {
       <div className="flex flex-col gap-1">
         <div className="mt-6">
           {user.bio && (
-            <blockquote className="prose prose-zinc mb-2 border-l border-zinc-500 bg-black/5 p-2 italic dark:prose-invert dark:bg-white/5">
+            <blockquote className="prose prose-zinc dark:prose-invert mb-2 border-l border-zinc-500 bg-black/5 p-2 italic dark:bg-white/5">
               {user.bio}
             </blockquote>
           )}
         </div>
-        <div className="flex gap-1">{renderSocialLinks(user)}</div>
+        <SocialComponent user={user} />
       </div>
     </main>
   );
