631774c76486aa0e404621e5be9e8a0148ea4366
btmask <90320947+adriangalilea@users.noreply.github.com>
2024-03-26 23:06:32 +0100

    refactor: improved typesafety on user-button

--- components/user-button.tsx
+++ components/user-button.tsx
@@ -1,3 +1,4 @@
+import { SelectUser } from "@/db/schema";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
 import Link from "next/link";
@@ -6,21 +7,21 @@ export default async function UserButton({
   username,
   image,
 }: {
-  username: string;
-  image: string;
+  username: SelectUser["username"];
+  image: SelectUser["image"];
 }) {
   return (
     <div className="flex items-center justify-around">
       <Button
         variant="ghost"
-        className="w-full rounded-none !p-0 transition-opacity ease-out hover:opacity-80"
+        className="w-full !p-0 transition-opacity ease-out hover:opacity-80"
         asChild
       >
         <Link href={`/${username}`} className="!p-0">
-          <Avatar className="!size-10 rounded-none">
-            <AvatarImage src={image} alt={username + " avatar"} />
+          <Avatar className="!size-10 ">
+            <AvatarImage src={image || ""} alt={username + " avatar"} />
             <AvatarFallback>
-              {username.trim().slice(0, 2).toUpperCase()}
+              {username ? username.trim().slice(0, 2).toUpperCase() : "ID"}
             </AvatarFallback>
           </Avatar>
         </Link>
