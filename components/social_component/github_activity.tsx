447365a792fb6533c4bba5fcb1aded78871b68db
btmask <90320947+adriangalilea@users.noreply.github.com>
2024-03-28 22:27:37 +0100

    chore: github activity cleanup

--- components/social_component/github_activity.tsx
+++ components/social_component/github_activity.tsx
@@ -1,23 +1,6 @@
 "use client";
 import GitHubCalendar, { ThemeInput } from "react-github-calendar";
 
-const selectLastHalfYear = (contributions: any) => {
-  const currentYear = new Date().getFullYear();
-  const currentMonth = new Date().getMonth();
-  const shownMonths = 6;
-
-  return contributions.filter((activity: any) => {
-    const date = new Date(activity.date);
-    const monthOfDay = date.getMonth();
-
-    return (
-      date.getFullYear() === currentYear &&
-      monthOfDay > currentMonth - shownMonths &&
-      monthOfDay <= currentMonth
-    );
-  });
-};
-
 const explicitTheme: ThemeInput = {
   light: [
     "hsl(190, 40%, 90%)",
@@ -34,7 +17,6 @@ const explicitTheme: ThemeInput = {
     "hsl(123, 55%, 52%)",
   ],
 };
-// "#383838", "#4D455D", "#7DB9B6", "#F5E9CF", "#E96479"
 
 export default function GitHubActivity({ username }: { username: string }) {
   return (
