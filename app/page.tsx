aefcac47b389ccd8be85847a32e8fba7ef66c7ba
btmask <90320947+adriangalilea@users.noreply.github.com>
2024-03-31 20:33:13 +0200

    fix: height in safari

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { LatestUsersTable } from "@/components/latest_users_table";
 export default async function Page() {
   const users = await getUsersWithUsername();
   return (
-    <div className="flex justify-between">
+    <div className="h-full flex justify-between">
       <LatestUsersTable users={users} />
     </div>
   );
