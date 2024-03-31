b330a60152c1b2dbdc42b5dc7662563720356f29
btmask <90320947+adriangalilea@users.noreply.github.com>
2024-04-01 01:38:31 +0200

    feat: sleep util

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// useful foor debugging Suspense and loading states
+export function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
