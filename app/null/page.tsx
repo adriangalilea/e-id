4bfb3afaee197e5def8380a8bbc824cff729e93a
btmask <90320947+adriangalilea@users.noreply.github.com>
2024-04-03 20:09:13 +0200

    feat: on /null successful renaming revalidation and redirection

--- app/null/page.tsx
+++ app/null/page.tsx
@@ -1,14 +1,22 @@
+export const dynamic = "force-dynamic";
+
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { auth } from "@/auth";
 import { TriangleAlert } from "lucide-react";
 import { ClientForm } from "./client_form";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 export default async function Page() {
   const session = await auth();
-  if (!session || !session.user || !session.user.id) return null;
-
-  if (session.user.username) redirect("/");
+  if (!session || !session.user || !session.user.id) redirect("/");
+  if (session.user.username && session.user.username_normalized) {
+    revalidatePath(`/${session.user.username_normalized}`);
+    revalidatePath(`/${session.user.username_normalized}/edit`);
+    revalidatePath(`/${session.user.username}`);
+    revalidatePath(`/${session.user.username}/edit`);
+    redirect(`/${session.user.username}`);
+  }
 
   return (
     <div className="flex flex-col justify-between gap-4">
