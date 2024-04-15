7716143815024e7af63c8d10c2dcfc92d2e9c791
btmask <90320947+adriangalilea@users.noreply.github.com>
2024-04-15 09:17:53 +0200

    fix: next-auth auth-components.tsx for next 14.2.1

--- components/auth-components.tsx
+++ components/auth-components.tsx
@@ -1,46 +1,75 @@
-import { signIn, signOut } from "@/auth";
+"use client";
+
 import { Button } from "./ui/button";
 import { LogOut, UserRound } from "lucide-react";
+
+import { signIn, signOut } from "next-auth/react";
 
 export function SignIn({
   provider,
   ...props
 }: { provider?: string } & React.ComponentPropsWithRef<typeof Button>) {
   return (
-    <form
-      className="w-full sm:w-fit"
-      action={async () => {
-        "use server";
-        await signIn(provider);
-      }}
+    <Button
+      aria-label="sign-in"
+      variant="ghost"
+      size="icon"
+      {...props}
+      onClick={() => signIn(provider)}
     >
-      <Button
-        variant="ghost"
-        className="!h-10 !w-10 !p-0"
-        {...props}
-        aria-label="sign-in"
-      >
-        <UserRound strokeWidth={1} />
-      </Button>
-    </form>
+      <UserRound strokeWidth={1} />
+    </Button>
   );
 }
+
 export function SignOut(props: React.ComponentPropsWithRef<typeof Button>) {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signOut();
-      }}
+    <Button
+      variant="destructiveGhost"
+      size="icon"
+      {...props}
+      aria-label="sign-in"
+      onClick={() => signOut()}
     >
-      <Button
-        variant="destructiveGhost"
-        className="!h-10 !w-10 !p-0"
-        {...props}
-        aria-label="sign-in"
-      >
-        <LogOut strokeWidth={1} />
-      </Button>
-    </form>
+      <LogOut strokeWidth={1} />
+    </Button>
   );
 }
+
+// export function SignIn({
+//   provider,
+//   ...props
+// }: { provider?: string } & React.ComponentPropsWithRef<typeof Button>) {
+//   return (
+//     <form
+//       className="w-full sm:w-fit"
+//       action={async () => {
+//         "use server";
+//         await signIn(provider);
+//       }}
+//     >
+//       <Button variant="ghost" className="!h-10 !w-10 !p-0" {...props}>
+// <UserRound strokeWidth={1} />
+//       </Button>
+//     </form>
+//   );
+// }
+// export function SignOut(props: React.ComponentPropsWithRef<typeof Button>) {
+//   return (
+//     <form
+//       action={async () => {
+//         "use server";
+//         await signOut();
+//       }}
+//     >
+//       <Button
+//         variant="destructiveGhost"
+//         className="!h-10 !w-10 !p-0"
+//         {...props}
+//         aria-label="sign-in"
+//       >
+//         <LogOut strokeWidth={1} />
+//       </Button>
+//     </form>
+//   );
+// }
