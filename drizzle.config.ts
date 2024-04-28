e13caf2342e2c9bf80b858732550d194c3ee16b6
adriangalilea <adriangalilea@gmail.com>
2024-04-28 19:45:42 +0200

    feat: drizzle dotenv for .local

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env.local" });
+
 export default defineConfig({
   schema: "./db/schema.ts",
   driver: "turso",
